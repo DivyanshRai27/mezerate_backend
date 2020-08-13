7735b16cbaa8c80c9cca838c170ae532129821c2
DivyanshRai27 <divyanshrai27@gmail.com>
2020-08-14 00:03:15 +0530

    deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +14,7 @@ app.get('/', (req,res) =>{
     res.render('index')
 })
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log("Server started on port 3000");
   });
   
