c068bb62d4f7fc87f5f754a2a9e099d686038e66
DivyanshRai27 <divyanshrai27@gmail.com>
2020-08-14 12:12:17 +0530

    added class

--- app.js
+++ app.js
@@ -10,9 +10,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
+let pages = __dirname + "/pages"
+
 app.get('/', (req,res) =>{
     res.render('index')
 })
+
+app.get('/test', (req,res) =>{
+    res.sendFile(pages+"/test.html")
+})
+
+app.get('/class10', (req,res) =>{
+    res.sendFile(pages+"/classes/class10/class10.html")
+})
+
 
 app.listen(port, function() {
     console.log("Server started on port 3000");
