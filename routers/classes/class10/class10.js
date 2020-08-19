92cfe499d58f568218f625833c84a6a7b9fb6178
DivyanshRai27 <divyanshrai27@gmail.com>
2020-08-19 20:35:08 +0530

    status

--- routers/classes/class10/class10.js
+++ routers/classes/class10/class10.js
@@ -4,7 +4,7 @@ const router = new express.Router()
 let pages = process.cwd() + '/pages/classes/class10'
 
 router.get('/class10', (req,res) =>{
-    res.render('classes/class10/class10',{Title:"Class 10th"})
+    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding Maths NCERT solutions"})
 })
 
 router.get('/class10/maths', (req,res) =>{
@@ -12,7 +12,7 @@ router.get('/class10/maths', (req,res) =>{
 })
 
 router.get('/class10/hindi', (req,res) =>{
-    res.render('classes/class10/hindi/hindi',{Title:"Hindi"})
+    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding Maths NCERT solutions"})
 })
 
 module.exports = router
