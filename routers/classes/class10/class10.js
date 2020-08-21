3c53c506bfc433231446c3e39e049c3d92213e8a
DivyanshRai27 <divyanshrai27@gmail.com>
2020-08-21 22:34:53 +0530

    added science

--- routers/classes/class10/class10.js
+++ routers/classes/class10/class10.js
@@ -4,15 +4,19 @@ const router = new express.Router()
 let pages = process.cwd() + '/pages/classes/class10'
 
 router.get('/class10', (req,res) =>{
-    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding Maths NCERT solutions"})
+    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding science NCERT solutions"})
 })
 
 router.get('/class10/maths', (req,res) =>{
-    res.render('classes/class10/maths/maths',{Title:"Maths",status:" "})
+    res.render('classes/class10/maths/maths',{Title:"Maths",status:"We are currently adding science NCERT solutions"})
 })
 
 router.get('/class10/hindi', (req,res) =>{
-    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding Maths NCERT solutions"})
+    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding science NCERT solutions"})
+})
+
+router.get('/class10/science', (req,res) =>{
+    res.render('classes/class10/science/science',{Title:"Science",status:" "})
 })
 
 module.exports = router
