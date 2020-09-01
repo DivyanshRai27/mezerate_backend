c9f340bad6e3e704390c607cfe060aa7c03b7e0b
DivyanshRai27 <divyanshrai27@gmail.com>
2020-09-01 20:43:09 +0530

    english

--- routers/classes/class10/class10.js
+++ routers/classes/class10/class10.js
@@ -4,19 +4,19 @@ const router = new express.Router()
 let pages = process.cwd() + '/pages/classes/class10'
 
 router.get('/class10', (req,res) =>{
-    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding science NCERT solutions"})
+    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding english material"})
 })
 
 router.get('/class10/maths', (req,res) =>{
-    res.render('classes/class10/maths/maths',{Title:"Maths",status:"We are currently adding science NCERT solutions"})
+    res.render('classes/class10/maths/maths',{Title:"Maths",status:"We are currently adding english material"})
 })
 
 router.get('/class10/hindi', (req,res) =>{
-    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding science NCERT solutions"})
+    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding english material"})
 })
 
 router.get('/class10/science', (req,res) =>{
-    res.render('classes/class10/science/science',{Title:"Science",status:" "})
+    res.render('classes/class10/science/science',{Title:"Science",status:"We are currently adding english material"})
 })
 
 router.get('/class10/english', (req,res) =>{
