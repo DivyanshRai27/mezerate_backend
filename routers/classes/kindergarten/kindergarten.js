c8c0887996bbd06e94a9ff1c1f208e246f2c7275
DivyanshRai27 <divyanshrai27@gmail.com>
2020-09-02 17:40:01 +0530

    added video

--- routers/classes/kindergarten/kindergarten.js
+++ routers/classes/kindergarten/kindergarten.js
@@ -5,4 +5,8 @@ router.get('/kindergarten', (req,res)=>{
     res.render('classes/kindergarten/kindergarten',{Title:"Kindergarten", status:" "})
 })
 
+router.get('/kindergarten/maths/counting', (req,res)=>{
+    res.render('classes/kindergarten/maths/counting',{Title:"Counting",status:" "})
+})
+
 module.exports = router
