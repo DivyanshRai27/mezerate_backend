4525f03af666dc3333e600a5677bc60603d8a26c
DivyanshRai27 <divyanshrai27@gmail.com>
2020-09-04 23:18:36 +0530

    added geography material

--- routers/classes/class10/class10.js
+++ routers/classes/class10/class10.js
@@ -4,23 +4,27 @@ const router = new express.Router()
 let pages = process.cwd() + '/pages/classes/class10'
 
 router.get('/class10', (req,res) =>{
-    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding english material"})
+    res.render('classes/class10/class10',{Title:"Class 10th",status:"We are currently adding Social Science material"})
 })
 
 router.get('/class10/maths', (req,res) =>{
-    res.render('classes/class10/maths/maths',{Title:"Maths",status:"We are currently adding english material"})
+    res.render('classes/class10/maths/maths',{Title:"Maths",status:"We are currently adding Social Science material"})
 })
 
 router.get('/class10/hindi', (req,res) =>{
-    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding english material"})
+    res.render('classes/class10/hindi/hindi',{Title:"Hindi",status:"We are currently adding Social Science material"})
 })
 
 router.get('/class10/science', (req,res) =>{
-    res.render('classes/class10/science/science',{Title:"Science",status:"We are currently adding english material"})
+    res.render('classes/class10/science/science',{Title:"Science",status:"We are currently adding Social Science material"})
 })
 
 router.get('/class10/english', (req,res) =>{
     res.render('classes/class10/english/english',{Title:"English",status:" "})
+})
+
+router.get('/class10/geography', (req,res) =>{
+    res.render('classes/class10/geography/geography',{Title:"Geography",status:" "})
 })
 
 router.get('/class10/exams', (req,res) =>{
